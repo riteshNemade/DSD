035ead6bf166e9f51cf90466fc64441ea2a1af1d
riteshNemade <ritesh.nemade@datafortune.com>
2023-10-23 18:13:06 +0530

    hide status bar in app build

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import RootNavigator from "./src/navigator/RootNavigator";
-import store from './src/redux/store'
+import store from "./src/redux/store";
 import { Provider } from "react-redux";
+import { StatusBar } from "expo-status-bar";
 export default function App() {
-
   return (
     <Provider store={store}>
-    <RootNavigator/>
+      <StatusBar translucent />
+      <RootNavigator />
     </Provider>
   );
 }
