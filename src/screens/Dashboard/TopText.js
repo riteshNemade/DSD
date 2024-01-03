3d3938687b09c79365bb519597c843c05ca48f98
riteshNemade <ritesh.nemade@datafortune.com>
2024-01-03 18:59:43 +0530

    UI scaling for company name

--- src/screens/Dashboard/TopText.js
+++ src/screens/Dashboard/TopText.js
@@ -1,14 +1,13 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { verticalScale } from "react-native-size-matters/extend";
-import { FONT_SIZE_LARGE, FONT_SIZE_REGULAR, colors, hPadding } from "../../constants/global";
+import { FONT_SIZE_REGULAR, colors, hPadding } from "../../constants/global";
 import { useSelector } from "react-redux";
 
 const TopText = ({ setIsModalVisible }) => {
   const company = useSelector((state) => {
     return state.global.companyName;
   });
-  console.log(company);
   return (
     <>
       {company !== "" && company !== undefined && company !== null ? (
@@ -19,22 +18,33 @@ const TopText = ({ setIsModalVisible }) => {
             flexDirection: "row",
           }}
         >
-          <Text style={{ fontSize: FONT_SIZE_REGULAR, fontWeight: 800 }}>Company: </Text>
-          <Text style={{ fontSize: FONT_SIZE_REGULAR, fontWeight: 400 }} numberOfLines={1}>
-            {company}{" "}
-          </Text>
-          <TouchableOpacity onPress={() => setIsModalVisible(true)}>
-            <Text
-              numberOfLines={1}
-              style={{
-                fontSize: FONT_SIZE_REGULAR-2,
-                fontWeight: 400,
-                color: colors.hyperlinkBlue,
-              }}
-            >
-              (change)
+          <View style={{}}>
+            <Text style={{ fontSize: FONT_SIZE_REGULAR, fontWeight: 800 }}>
+              Company:{" "}
             </Text>
-          </TouchableOpacity>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text
+              style={{ fontSize: FONT_SIZE_REGULAR, fontWeight: 400 }}
+              numberOfLines={1}
+            >
+              {company}{" "}
+            </Text>
+          </View>
+          <View style={{}}>
+            <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+              <Text
+                numberOfLines={1}
+                style={{
+                  fontSize: FONT_SIZE_REGULAR - 2,
+                  fontWeight: 400,
+                  color: colors.hyperlinkBlue,
+                }}
+              >
+                (change)
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       ) : (
         <></>
@@ -44,5 +54,3 @@ const TopText = ({ setIsModalVisible }) => {
 };
 
 export default TopText;
-
-const styles = StyleSheet.create({});
